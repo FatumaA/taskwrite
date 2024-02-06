f54d0b62a811bb07b448b6ee438d88e5feeb4387
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-02-07 00:49:21 +0300

    build: connect to db

--- src/utils/appwrite.ts
+++ src/utils/appwrite.ts
@@ -1,9 +1,11 @@
-import { Client } from "appwrite";
+import { Client, Databases } from "appwrite";
 
 export const client = new Client();
 
 client
 	.setEndpoint(import.meta.env.VITE_APPWRITE_URL)
 	.setProject(import.meta.env.VITE_APPWRITE_PROJ_ID);
+
+export const databases = new Databases(client);
 
 export { ID } from "appwrite";
