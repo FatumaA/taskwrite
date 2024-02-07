5322dee9fd3e2fc944ba41843916bbb4c579b311
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-02-07 18:50:13 +0300

    feat: get read to work with typescript

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,15 +1,39 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { readDocuments } from "../db/db";
+import { Models } from "appwrite";
+import { ITask } from "../models/interface";
 // import { ArrowDown } from "@appwrite.io/pink-icons";
 
 const Task = () => {
+	const [tasks, setTasks] = useState<ITask[]>([]);
+
+	const getTasks = async () => {
+		const { documents } = await readDocuments();
+		console.log("ALL TASKS", documents);
+
+		return documents as ITask[];
+	};
+
+	useEffect(() => {
+		getTasks()
+			.then((res) => {
+				setTasks(res);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
 	return (
 		<div>
-			<section>
-				<h2>Task Title</h2>
-				<p>Task Description</p>
-				<span>Task Due Date</span>
-			</section>
-			{/* <ArrowDown /> */}
+			{tasks!.map((task: Models.Document) => {
+				return (
+					<section key={task.$id}>
+						<h2>{task.title}</h2>
+						<p>{task.description}</p>
+						<span>{task.due_date}</span>
+					</section>
+				);
+			})}
 		</div>
 	);
 };
