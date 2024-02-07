2a2dc8f3694312b716eb3e8fdeb1a6726fcf4434
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-02-07 18:56:07 +0300

    feat: get create to work

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import { createDocument } from "../db/db";
+import { IPayload } from "../models/interface";
 
 const AddTask = () => {
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const title = (e.currentTarget.elements[0] as HTMLInputElement).value;
+		const description = (e.currentTarget.elements[1] as HTMLTextAreaElement)
+			.value;
+		const date = (e.currentTarget.elements[2] as HTMLInputElement).value;
+
+		const payload: IPayload = {
+			title,
+			description,
+			due_date: date,
+		};
+
+		const res = await createDocument(payload);
+
+		const form = document.getElementById("form") as HTMLFormElement;
+
+		form.reset();
+
+		console.log("FORM TEST", res);
+	};
+
 	return (
-		<form>
+		<form id="form" onSubmit={handleSubmit}>
 			<div>
 				<label htmlFor="title">Task Title</label>
 				<input type="text" id="title" placeholder="Title of your task" />
