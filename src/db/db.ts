d357083f1f215780f311cb8c879acb37ebebd8d2
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-02-07 18:49:29 +0300

    refactor: move exports to bottom of file

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,15 +1,10 @@
 import { ID, databases } from "../utils/appwrite";
+import { IPayload } from "../models/interface";
 
 const dbID = import.meta.env.VITE_APPWRITE_DB_ID;
 const collectionID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
-interface IPayload {
-	title: string;
-	description: string;
-	due_date: Date;
-}
-
-export const createDocument = async (payload: IPayload) => {
+const createDocument = async (payload: IPayload) => {
 	const res = await databases.createDocument(dbID, collectionID, ID.unique(), {
 		...payload,
 	});
@@ -17,21 +12,23 @@ export const createDocument = async (payload: IPayload) => {
 	return res;
 };
 
-export const readDocuments = async () => {
+const readDocuments = async () => {
 	const res = await databases.listDocuments(dbID, collectionID);
 
 	return res;
 };
 
-export const updateDocument = async (payload: IPayload, id: string) => {
+const updateDocument = async (payload: IPayload, id: string) => {
 	const res = await databases.updateDocument(dbID, collectionID, id, {
 		...payload,
 	});
 
 	return res;
 };
-export const deleteDocument = async (id: string) => {
+const deleteDocument = async (id: string) => {
 	const res = await databases.deleteDocument(dbID, collectionID, id);
 
 	return res;
 };
+
+export { createDocument, readDocuments, updateDocument, deleteDocument };
