e1e8c55303126c4a80a10f00e5916c4896313d3a
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-02-23 03:30:32 +0300

    style: clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Task from "./components/Task";
 
 function App() {
 	return (
-		<main className="container mx-auto border border-red-500">
-			<section className="max-w-5xl mx-auto m-24 p-0 md:p-16 border border-purple-500">
-				<h1 className="text-5xl md:text-6xl text-center py-8">
+		<main className="container mx-auto">
+			<section className="max-w-5xl mx-auto m-12 p-0 md:p-16">
+				<h1 className="text-5xl md:text-6xl font-bold text-center py-3">
 					Taskwrite - Manage your tasks
 				</h1>
 				<AddTask task={null} isEdit={false} />
