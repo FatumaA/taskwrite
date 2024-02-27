5da3a5820dd1bb6fa6f8291ec3ca7b38fad9400d
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-02-27 16:33:21 +0300

    fix: pull speech-to-text logic to helper hook

---
+++ src/hooks/useSpeechToTextHelper.ts
@@ -0,0 +1,20 @@
+import "regenerator-runtime/runtime";
+import { useState } from "react";
+import { useSpeechRecognition } from "react-speech-recognition";
+
+export function useSpeechToTextHelper() {
+	const [error, setError] = useState("");
+
+	const {
+		transcript,
+		listening,
+		resetTranscript,
+		browserSupportsSpeechRecognition,
+	} = useSpeechRecognition();
+
+	if (!browserSupportsSpeechRecognition) {
+		setError("Browser doesn't support speech recognition.");
+	}
+
+	return { error, transcript, listening, resetTranscript };
+}
