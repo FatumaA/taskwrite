a5dac80fd2f043b103cdd8d016bacac9c4e72572
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-01 18:26:54 +0300

    feat: display dialog properly on edit

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
 import { deleteDocument, readDocuments, updateDocument } from "../db/db";
 import { IPayload, ITask } from "../models/interface";
-import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
+import {
+	PencilSquareIcon,
+	TrashIcon,
+	XMarkIcon,
+} from "@heroicons/react/24/solid";
 import AddTask from "./AddTask";
 import Button from "./Button";
 
@@ -22,6 +26,7 @@ const Task = () => {
 			title: currentTask.title,
 			description: currentTask.description,
 			due_date: currentTask.due_date,
+			priority: currentTask.priority,
 			done: isDone,
 		};
 
@@ -39,6 +44,8 @@ const Task = () => {
 		console.log("Edit", currentTask);
 		setIsEdit(true);
 		setSelectedTask(currentTask);
+		// return <AddTask key={currentTask.$id} task={currentTask} isEdit={true} />;
+		// navigate("/", { state: { isEdit: true, task: selectedTask } });
 	};
 
 	const handleDelete = async (currentTaskId: string) => {
@@ -56,89 +63,100 @@ const Task = () => {
 				console.error(err);
 			});
 	}, []);
+
 	return (
-		<div>
-			{tasks.map((task: ITask) => {
-				return (
-					<div className="m-8 border border-gray-400 rounded-md p-4 hover:shadow-lg transition duration-300 ease-in-out">
-						{task.priority && (
-							<span>
-								<span className="font-medium">Task priority: </span>
-								<span
-									className={`${
-										task.priority === "low"
-											? "bg-yellow-400"
-											: task.priority === "medium"
-											? "bg-orange-400"
-											: "bg-red-400"
-									} py-1 px-2 rounded-md`}
-								>
-									{task.priority}
-								</span>
-							</span>
-						)}
-
-						<section
-							key={task.$id}
-							className="flex flex-col md:flex-row justify-between gap-5"
-						>
-							<section>
-								<h2 className="text-2xl font-medium py-2">{task.title}</h2>
-								<p className="py-1 mb-5">{task.description}</p>
-
-								<span className="font-extralight text-gray-600">
-									<span className="font-medium">Due on: </span>
-									<span className="underline">{`${new Date(
-										task.due_date
-									).toLocaleDateString()}`}</span>
-								</span>
-							</section>
-							<section className="flex flex-col justify-between">
-								<div className="flex gap-2 py-1">
-									<Button
-										bgColor="bg-green-400"
-										text="Edit"
-										icon={PencilSquareIcon}
-										iconClasses="hidden md:flex"
-										handleClick={() => handleEdit(task)}
-									/>
-									<Button
-										bgColor="bg-red-400"
-										text="Delete"
-										icon={TrashIcon}
-										iconClasses="hidden md:flex"
-										handleClick={() => handleDelete(task.$id)}
-									/>
-								</div>
-								<div className="flex items-center">
-									<label htmlFor="done" className="mr-2 font-light">
-										Mark as complete
-									</label>
-									<input
-										type="checkbox"
-										onChange={(e) => {
-											setIsDone(e.target.checked);
-											handleCheckbox(task, task.$id);
-										}}
-										checked={task.done}
-										className="size-5 accent-pink-600 rounded-sm"
-									/>
-								</div>
-							</section>
-						</section>
-					</div>
-				);
-			})}
+		<main className="container mx-auto">
 			{isEdit && (
-				// <AddTask key={selectedTask.$id} task={selectedTask} isEdit={true} />
-				<dialog open={true} id="modal">
-					<AddTask key={selectedTask!.$id} task={selectedTask!} isEdit={true} />
-					<button onClick={closeModal} id="closeModal">
-						Close
+				<dialog
+					open={true}
+					id="modal"
+					className="p-8 fixed inset-0 backdrop-filter backdrop-blur-md backdrop-brightness-50 w-4/6 border border-gray-400 rounded-md"
+				>
+					<button
+						onClick={closeModal}
+						id="closeModal"
+						className="flex gap-2 ml-auto font-medium hover:text-red-500"
+					>
+						<span>Close </span>
+						<XMarkIcon height={25} />
 					</button>
+					<AddTask key={selectedTask!.$id} task={selectedTask!} isEdit={true} />
 				</dialog>
 			)}
-		</div>
+			<section className="max-w-5xl mx-auto m-12 p-0 md:p-16">
+				{tasks.map((task: ITask) => {
+					return (
+						<div className="m-8 border border-gray-400 rounded-md p-4 hover:shadow-lg transition duration-300 ease-in-out">
+							{task.priority && (
+								<span>
+									<span className="font-medium">Task priority: </span>
+									<span
+										className={`${
+											task.priority === "low"
+												? "bg-yellow-400"
+												: task.priority === "medium"
+												? "bg-orange-400"
+												: "bg-red-400"
+										} py-1 px-2 rounded-md`}
+									>
+										{task.priority}
+									</span>
+								</span>
+							)}
+
+							<section
+								key={task.$id}
+								className="flex flex-col md:flex-row justify-between gap-5"
+							>
+								<section>
+									<h2 className="text-2xl font-medium py-2">{task.title}</h2>
+									<p className="py-1 mb-5">{task.description}</p>
+
+									<span className="font-extralight text-gray-600">
+										<span className="font-medium">Due on: </span>
+										<span className="underline">{`${new Date(
+											task.due_date
+										).toLocaleDateString()}`}</span>
+									</span>
+								</section>
+								<section className="flex flex-col justify-between">
+									<div className="flex gap-2 py-1">
+										<Button
+											bgColor="bg-green-400"
+											text="Edit"
+											icon={PencilSquareIcon}
+											iconClasses="hidden md:flex"
+											handleClick={() => handleEdit(task)}
+										/>
+										<Button
+											bgColor="bg-red-400"
+											text="Delete"
+											icon={TrashIcon}
+											iconClasses="hidden md:flex"
+											handleClick={() => handleDelete(task.$id)}
+										/>
+									</div>
+									<div className="flex items-center">
+										<label htmlFor="done" className="mr-2 font-light">
+											Mark as complete
+										</label>
+										<input
+											type="checkbox"
+											onChange={(e) => {
+												setIsDone(e.target.checked);
+												handleCheckbox(task, task.$id);
+											}}
+											checked={task.done}
+											className="size-5 accent-pink-600 rounded-sm"
+										/>
+									</div>
+								</section>
+							</section>
+						</div>
+					);
+				})}
+			</section>
+		</main>
 	);
 };
 
