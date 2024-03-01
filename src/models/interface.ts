8ceac94beea38b517fb5d42c5e9e7d0fb7c04664
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-01 10:52:02 +0300

    chore: update interface due_date filed type

--- src/models/interface.ts
+++ src/models/interface.ts
@@ -3,13 +3,13 @@ import { Models } from "appwrite";
 export interface IPayload {
 	title: string;
 	description: string;
-	due_date: string;
+	due_date: Date;
 	done?: boolean;
 }
 
 export interface ITask extends Models.Document {
 	title: string;
 	description: string;
-	due_date: string;
+	due_date: Date;
 	done: boolean;
 }
