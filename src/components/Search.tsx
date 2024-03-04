0aff6aa4133327e2c46359d5a902cf8defd07e37
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-04 12:00:59 +0300

    feat: pull search into its own component

---
+++ src/components/Search.tsx
@@ -0,0 +1,74 @@
+import { FormEvent, useState } from "react";
+import { ITask } from "../models/interface";
+import Dialog from "./Dialog";
+import TaskItem from "./TaskItem";
+
+interface SearchProps {
+	tasks: ITask[];
+}
+
+const Search = ({ tasks }: SearchProps) => {
+	const [searchTerm, setSearchTerm] = useState("");
+	const [searchedTasks, setSearchedTasks] = useState<ITask[]>([]);
+	const [error, setError] = useState("");
+
+	const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		if (!searchTerm) {
+			setError("No search term entered");
+			setTimeout(() => {
+				setError("");
+			}, 3000);
+			return;
+		}
+
+		const filteredTasks = tasks.filter(
+			(task) =>
+				task.title.includes(searchTerm) || task.description.includes(searchTerm)
+		);
+		if (filteredTasks.length === 0) {
+			setError("No task found");
+			setTimeout(() => {
+				setSearchTerm("");
+				setError("");
+			}, 3000);
+			return;
+		}
+		setSearchedTasks(filteredTasks);
+	};
+	return (
+		<>
+			<form className="flex items-center gap-2 w-1/2" onSubmit={handleSubmit}>
+				{searchedTasks.length > 0 && (
+					<Dialog setSearchedTasks={setSearchedTasks}>
+						{searchedTasks.map((task: ITask) => (
+							<TaskItem key={task.$id} task={task} />
+						))}
+					</Dialog>
+				)}
+				<input
+					aria-roledescription="search"
+					type="text"
+					id="search"
+					placeholder="search your tasks..."
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
+					className={`border rounded-md p-2 focus:outline-none focus:ring-1 ${
+						error
+							? "border-red-600 focus:ring-red-500 invalid:focus:ring-red-600"
+							: "border-slate-800 focus:ring-slate-900"
+					}`}
+				/>
+				<button
+					className="rounded-md px-2 py-2 hover:scale-105 transition duration-300 ease-in-out bg-pink-700 text-white hover:bg-pink-800 font-medium"
+					type="submit"
+				>
+					Search
+				</button>
+			</form>
+			<span className="text-red-500 font-medium mt-1">{error}</span>
+		</>
+	);
+};
+
+export default Search;
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,17 +1,16 @@
-import { useState, useEffect, FormEvent } from "react";
+import { useState, useEffect } from "react";
 import { readDocuments } from "../db/db";
 import { ITask } from "../models/interface";
 import TaskItem from "./TaskItem";
 import Button from "./Button";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { useNavigate } from "react-router-dom";
-import Dialog from "./Dialog";
+import Search from "./Search";
 
 const Task = () => {
 	const [tasks, setTasks] = useState<ITask[]>([]);
-	const [searchedTasks, setSearchedTasks] = useState<ITask[]>([]);
-	const [searchTerm, setSearchTerm] = useState("");
-	const [error, setError] = useState("");
+	// const [searchedTasks, setSearchedTasks] = useState<ITask[]>([]);
+
 	const navigate = useNavigate();
 
 	const getTasks = async () => {
@@ -19,30 +18,6 @@ const Task = () => {
 		console.log("ALL TASKS", documents);
 
 		return documents as ITask[];
-	};
-
-	const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
-		e.preventDefault();
-		if (!searchTerm) {
-			setError("No search term entered");
-			setTimeout(() => {
-				setError("");
-			}, 3000);
-			return;
-		}
-
-		const filteredTasks = tasks.filter(
-			(task) =>
-				task.title.includes(searchTerm) || task.description.includes(searchTerm)
-		);
-		if (filteredTasks.length === 0) {
-			setError("No task found");
-			setTimeout(() => {
-				setError("");
-			}, 3000);
-			return;
-		}
-		setSearchedTasks(filteredTasks);
 	};
 
 	useEffect(() => {
@@ -63,33 +38,8 @@ const Task = () => {
 				</h1>
 				<div className="m-8 flex justify-between items-center">
 					<div className="flex flex-col">
-						<form
-							className="flex items-center gap-2 w-1/2"
-							onSubmit={handleSubmit}
-						>
-							<input
-								aria-roledescription="search"
-								type="text"
-								id="search"
-								placeholder="search your tasks..."
-								value={searchTerm}
-								onChange={(e) => setSearchTerm(e.target.value)}
-								className={`border rounded-md p-2 focus:outline-none focus:ring-1 ${
-									error
-										? "border-red-600 focus:ring-red-500 invalid:focus:ring-red-600"
-										: "border-slate-800 focus:ring-slate-900"
-								}`}
-							/>
-							<button
-								className="rounded-md px-2 py-2 hover:scale-105 transition duration-300 ease-in-out bg-pink-700 text-white hover:bg-pink-800 font-medium"
-								type="submit"
-							>
-								Search
-							</button>
-						</form>
-						<span className="text-red-500 font-medium mt-1">{error}</span>
+						<Search tasks={tasks} />
 					</div>
-
 					<Button
 						bgColor="bg-pink-700 text-white font-medium py-2 hover:bg-pink-800"
 						text="Add Task"
@@ -102,13 +52,13 @@ const Task = () => {
 					/>
 				</div>
 				<div className="flex flex-col md:flex-row justify-between">
-					{searchedTasks.length > 0 && (
+					{/* {searchedTasks.length > 0 && (
 						<Dialog setSearchedTasks={setSearchedTasks}>
 							{searchedTasks.map((task: ITask) => (
 								<TaskItem key={task.$id} task={task} />
 							))}
 						</Dialog>
-					)}
+					)} */}
 					<div className="flex-1">
 						<h3 className="text-2xl font-bold m-8">Pending Tasks</h3>
 						{tasks.map((task: ITask) => {
