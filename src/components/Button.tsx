1c765a886d587e9d5f6c0a1029d971efd18625e7
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-05 15:40:20 +0300

    style: update button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,21 @@
 interface ButtonProps {
-	bgColor: string;
+	extraBtnClasses?: string;
 	textColor?: string;
 	text?: string;
 	icon?: React.ElementType;
 	iconClasses?: string;
+	textClasses?: string;
 	handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 	title?: string;
 }
 
 function Button({
-	bgColor,
+	extraBtnClasses,
 	textColor,
 	text,
 	icon,
 	iconClasses,
+	textClasses,
 	handleClick,
 	title,
 }: ButtonProps) {
@@ -22,11 +24,11 @@ function Button({
 		<button
 			onClick={handleClick}
 			title={title ?? ""}
-			className={`flex items-center ${bgColor} ${
+			className={`flex items-center ${extraBtnClasses} ${
 				textColor ?? null
 			} rounded-md px-2 py-1 hover:scale-105 transition duration-300 ease-in-out`}
 		>
-			<span className="mr-1">{text}</span>
+			<span className={`${textClasses} mr-1`}>{text}</span>
 			{Icon && <Icon height={25} className={iconClasses} />}
 		</button>
 	);
