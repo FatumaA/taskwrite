91863e6ca4b08a24ccacd2c68f4af87912365043
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-05 23:45:46 +0300

    chore: clean up

--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -3,7 +3,6 @@ import { ITask } from "../models/interface";
 
 export const getTasks = async () => {
 	const { documents } = await readDocuments();
-	console.log("ALL TASKS", documents);
 
 	return documents as ITask[];
 };
