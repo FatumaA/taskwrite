b5d506c4e32df4ec5f0f359b9e5cf3bbe8e7c497
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-07 13:55:20 +0300

    fix: require a text or icon for button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,34 +1,51 @@
+type ButtonContent =
+	| {
+			text: string;
+			icon?: React.ElementType;
+	  }
+	| {
+			text?: string;
+			icon: React.ElementType;
+	  };
+
 interface ButtonProps {
 	extraBtnClasses?: string;
 	textColor?: string;
-	text?: string;
-	icon?: React.ElementType;
+	content: ButtonContent;
 	iconClasses?: string;
 	textClasses?: string;
-	handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+	handleClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 	title?: string;
+	disable?: boolean;
+	type?: "button" | "submit" | "reset";
 }
 
 function Button({
 	extraBtnClasses,
 	textColor,
-	text,
-	icon,
+	content: { text, icon },
 	iconClasses,
 	textClasses,
 	handleClick,
 	title,
+	disable,
+	type = "button",
 }: ButtonProps) {
 	const Icon = icon;
+
+	const handleClickProp = type === "submit" ? undefined : handleClick;
+
 	return (
 		<button
-			onClick={handleClick}
+			type={type}
 			title={title ?? ""}
+			onClick={handleClickProp}
+			disabled={disable}
 			className={`flex items-center text-iconColor ${extraBtnClasses} ${
 				textColor ?? ""
 			} rounded-md px-2 py-1 hover:scale-105 transition duration-300 ease-in-out`}
 		>
-			<span className={`${textClasses} mr-1`}>{text}</span>
+			<span className={`${textClasses} mr-1`}>{text ?? ""}</span>
 			{Icon && <Icon height={25} className={iconClasses} />}
 		</button>
 	);
