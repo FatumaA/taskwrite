b797caccfe78895b19438fdedf1974eb9728d3c8
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-08 01:56:40 +0300

    refactor: update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
 import "./index.css";
-import AddTask from "./components/AddTask";
+// import AddTask from "./components/AddTask";
+// import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import { ITask } from "./models/interface";
+import Task from "./routes/Task";
+import Index from "./routes/Index";
 import Navbar from "./components/Navbar";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { ITask } from "./models/interface";
 
 function App() {
-	const location = useLocation();
-	const navigate = useNavigate();
-
-	const taskFromState: ITask = location.state?.task;
-
-	const [taskToEdit] = useState<ITask | null>(taskFromState ?? null);
-
-	useEffect(() => {
-		navigate("/", { state: undefined });
-	}, [navigate, taskFromState]);
 	return (
 		<>
 			<Navbar resetNavigation={true} />
-			<main className="container mx-auto">
-				<section className="max-w-5xl mx-auto m-12 p-16">
-					<h1 className="text-4xl md:text-7xl font-bold text-center py-3 mb-16">
-						AI-enhanced, Voice-enabled, Searchable Task Manager
-					</h1>
-					<AddTask task={taskToEdit} isEdit={taskToEdit ? true : false} />
-				</section>
-			</main>
+			<Routes>
+				<Route path="/" element={<Index />} />
+				<Route path="/tasks" element={<Task />} />
+			</Routes>
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import Task from "./routes/Task.tsx";
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
-	{
-		path: "/tasks",
-		element: <Task />,
-	},
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 );
