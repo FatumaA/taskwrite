0fb3d68c8e01753ec391b54993e02870847cfb82
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-07 15:44:45 +0300

    refactor: simplify sort logic, add error state

--- src/routes/Task.tsx
+++ src/routes/Task.tsx
@@ -11,64 +11,61 @@ import Navbar from "../components/Navbar";
 
 const Task = () => {
 	const [tasks, setTasks] = useState<ITask[]>([]);
+	const [tasksError, setTasksError] = useState("");
 
 	const navigate = useNavigate();
 
 	const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
 		e.preventDefault();
 
+		const doneTasks = tasks.filter((task) => task.done);
 		const pendingTasks = tasks.filter((task) => !task.done);
+
+		const mapPriorityToValue = (priority: string | undefined): number => {
+			const priorityValues: { [key: string]: number } = {
+				low: 0,
+				medium: 1,
+				high: 2,
+			};
+
+			if (!priority || !(priority in priorityValues)) {
+				return 0;
+			}
+
+			return priorityValues[priority];
+		};
+		const sortTasksByPriority = (isAscending: boolean) => {
+			return pendingTasks.sort((a, b) => {
+				const aValue = mapPriorityToValue(a.priority);
+				const bValue = mapPriorityToValue(b.priority);
+				return isAscending ? aValue - bValue : bValue - aValue;
+			});
+		};
+
+		const sortTasksByDueDate = (isEarly: boolean) => {
+			return pendingTasks.sort((a, b) => {
+				const aDate = new Date(a.due_date).getTime();
+				const bDate = new Date(b.due_date).getTime();
+				return isEarly ? aDate - bDate : bDate - aDate;
+			});
+		};
 
 		switch (e.target.value) {
 			case "priority - (low - high)":
-				setTasks([
-					...tasks.filter((task) => task.done),
-					...pendingTasks.sort(
-						(a, b) =>
-							mapPriorityToValue(a.priority) - mapPriorityToValue(b.priority)
-					),
-				]);
+				setTasks([...doneTasks, ...sortTasksByPriority(true)]);
 				break;
 			case "priority - (high - low)":
-				setTasks([
-					...tasks.filter((task) => task.done),
-					...pendingTasks.sort(
-						(a, b) =>
-							mapPriorityToValue(b.priority) - mapPriorityToValue(a.priority)
-					),
-				]);
+				setTasks([...doneTasks, ...sortTasksByPriority(false)]);
 				break;
 			case "due date - (earliest - latest)":
-				setTasks([
-					...tasks.filter((task) => task.done),
-					...pendingTasks.sort(
-						(a, b) =>
-							new Date(a.due_date).getTime() - new Date(b.due_date).getTime()
-					),
-				]);
+				setTasks([...doneTasks, ...sortTasksByDueDate(true)]);
 				break;
 			case "due date - (latest - earliest)":
-				setTasks([
-					...tasks.filter((task) => task.done),
-					...pendingTasks.sort(
-						(a, b) =>
-							new Date(b.due_date).getTime() - new Date(a.due_date).getTime()
-					),
-				]);
+				setTasks([...doneTasks, ...sortTasksByDueDate(false)]);
 				break;
 			default:
 				break;
 		}
-	};
-
-	const mapPriorityToValue = (priority: string | undefined): number => {
-		const priorityValues: { [key: string]: number } = {
-			low: 0,
-			medium: 1,
-			high: 2,
-		};
-
-		return priorityValues[priority || ""] || 0;
 	};
 
 	const selectArray = [
@@ -85,6 +82,7 @@ const Task = () => {
 			})
 			.catch((err) => {
 				console.error(err);
+				setTasksError("Error fetching tasks, please try again");
 			});
 	}, []);
 
@@ -108,49 +106,48 @@ const Task = () => {
 							}}
 						/>
 					</div>
-
-					<div className="flex flex-col md:flex-row justify-between">
-						<div className="flex-1">
-							<h3 className="text-2xl font-bold m-8">Pending Tasks</h3>
-							<div className="m-8 flex items-start lg:items-center gap-1 justify-between flex-col lg:flex-row">
-								<span className="font-medium">Sort Tasks by: </span>
-								<Select
-									defaultSelectValue={selectArray[0]}
-									handleSelectChange={handleSelectChange}
-									selectOptions={selectArray}
-								/>
-							</div>
-							<div>
-								{tasks.map((task: ITask) => {
-									if (!task.done) {
-										return (
+					{tasksError ? (
+						<span className="m-8 text-error">{tasksError}</span>
+					) : (
+						<div className="flex flex-col md:flex-row justify-between">
+							<div className="flex-1">
+								<h3 className="text-2xl font-bold m-8">Pending Tasks</h3>
+								<div className="m-8 flex items-start lg:items-center gap-1 justify-between flex-col lg:flex-row">
+									<span className="font-medium">Sort Tasks by: </span>
+									<Select
+										defaultSelectValue={selectArray[0]}
+										handleSelectChange={handleSelectChange}
+										selectOptions={selectArray}
+									/>
+								</div>
+								<div>
+									{tasks
+										.filter((task) => !task.done)
+										.map((task) => (
 											<TaskItem
 												key={task.$id}
 												task={task}
 												setTasks={setTasks}
 											/>
-										);
-									}
-								})}
+										))}
+								</div>
 							</div>
-						</div>
-						<div className="flex-1">
-							<h3 className="text-2xl font-bold m-8">Completed Tasks</h3>
-							<div>
-								{tasks.map((task: ITask) => {
-									if (task.done === true) {
-										return (
+							<div className="flex-1">
+								<h3 className="text-2xl font-bold m-8">Completed Tasks</h3>
+								<div>
+									{tasks
+										.filter((task) => task.done)
+										.map((task) => (
 											<TaskItem
 												key={task.$id}
 												task={task}
 												setTasks={setTasks}
 											/>
-										);
-									}
-								})}
+										))}
+								</div>
 							</div>
 						</div>
-					</div>
+					)}
 				</section>
 			</main>
 		</>
