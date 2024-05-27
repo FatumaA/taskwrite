a92a164832116fae4a7fd27c587564d45588895a
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-05-27 16:33:09 +0300

    Revert "Accepted changes from PR #2 updating AI model"
    This reverts commit cb86ce6ee36f3ad9fc6e6cc4926556c3b2bf7ddd.

--- src/utils/ai.ts
+++ src/utils/ai.ts
@@ -1,4 +1,5 @@
 import { HfInference } from "@huggingface/inference";
+import { HuggingFaceStream, StreamingTextResponse } from "ai";
 
 // Create a new HuggingFace Inference instance
 const Hf = new HfInference(import.meta.env.VITE_HUGGINGFACE_KEY);
@@ -7,21 +8,22 @@ const Hf = new HfInference(import.meta.env.VITE_HUGGINGFACE_KEY);
 export const runtime = "edge";
 
 export const callAI = async (prompt: string) => {
-	const response = await Hf.textGeneration({
-		model: "mistralai/Mistral-7B-Instruct-v0.3", // Replace with a text-generation model
+	const response = Hf.textGenerationStream({
+		model: "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
 		inputs: `<|prompter|>${prompt}<|endoftext|><|assistant|>`,
 		parameters: {
 			max_new_tokens: 200,
-			repetition_penalty: 1.0,
+			// @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
+			typical_p: 0.2,
+			repetition_penalty: 1,
 			truncate: 1000,
 			return_full_text: false,
 		},
 	});
 
 	// Convert the response into a friendly text-stream
-	// const stream = HuggingFaceStream(response);
+	const stream = HuggingFaceStream(response);
 
 	// Respond with the stream
-	// return new StreamingTextResponse(stream);
-	return response;
+	return new StreamingTextResponse(stream);
 };
